38df067eda132baf8379d58971faaeef255ba0d9
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-03-30 12:52:59 -0300

    feat: create Contact schema with mongoose

---
+++ src/database/schemas/Contact.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const Contact = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+export default mongoose.model('User', Contact);
