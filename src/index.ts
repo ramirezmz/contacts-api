b81e89b7e3e9754681bfe73e631dd41fd01f6d4d
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-03-30 12:39:23 -0300

    feat: create basic server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
 
-export function double(x: number): number {
-  return x * 2;
-}
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+const URL = process.env.URL || 'http://localhost'
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${URL}:${PORT}`)
+})
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
