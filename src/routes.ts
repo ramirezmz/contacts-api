7fcab4d86331fa4fd0caf844e24c3361bd7ffdc6
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-03-31 09:24:09 -0300

    🔥feat: created routes separed to main

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import UserController from './controller/UserController';
+
+const router = Router();
+
+router.get('/health', UserController.healthCheck)
+router.post('/user', UserController.create);
+router.get('/users', UserController.findAll)
+
+export default router
