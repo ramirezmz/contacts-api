4e3d553c379d8011ff8e6c9968d4b37452133f7c
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-03-31 08:33:42 -0300

    ♻refactor: move to other file routes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import router from './routes'
 
 dotenv.config()
-
 const app = express()
 const PORT = process.env.PORT || 3000
 const URL = process.env.URL || 'http://localhost'
+const DB_NAME = process.env.DB_NAME || 'sistema-api-local'
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`)
 
 app.use(express.json())
-
+app.use(router)
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${URL}:${PORT}`)
-})
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(`Server is running on port ${URL}:${PORT} 🚀`)
 })
