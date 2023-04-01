c1a14464a1fe672885ca293f11157a8fce4c7504
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-04-01 00:34:48 -0300

    🔥feat: create router to delete user by id

--- src/routes.ts
+++ src/routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/health', UserController.healthCheck)
 router.post('/user', UserController.create);
 router.get('/users', UserController.findAll)
+router.delete('/user/:id', UserController.delete)
 
 export default router
