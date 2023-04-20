7a9fd988a9b08108e1f6063a5999d54b989125ce
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-04-20 16:58:56 -0300

    ⚡️ inprove port to Heroku

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,12 @@ dotenv.config()
 const app = express()
 const URL = process.env.APP_URL || process.env.LOCAL_URL
 const MONGO_URL = process.env.MONGO_URL || process.env.LOCAL_MONGO_URL
+const PORT = process.env.PORT || 3000
 
 mongoose.connect(`${MONGO_URL}`)
 
 app.use(express.json())
 app.use(router)
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on ${URL} 🚀`)
 })
